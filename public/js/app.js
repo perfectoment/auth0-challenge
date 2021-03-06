308e142afeb8d93cfefdaa5b3875865d4f6f10c7
Lee Marvin <ryenem8@gmail.com>
2021-03-06 14:00:21 -0800

    added logout functionality

--- public/js/app.js
+++ public/js/app.js
@@ -18,11 +18,56 @@ window.onload = async () => {
 
     updateUI();
 
-  };
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+
+        return;
+    }
+
+  const query = window.location.search;
+  if (query.includes("code=") && query.includes("state=")) {
+
+    await auth0.handleRedirectCallback();
+
+    updateUI();
+
+
+    window.history.replaceState({}, document.title, "/");
+  }
+
+};
 
   const updateUI = async () => {
     const isAuthenticated = await auth0.isAuthenticated();
   
     document.getElementById("btn-logout").disabled = !isAuthenticated;
     document.getElementById("btn-login").disabled = isAuthenticated;
+
+    if (isAuthenticated) {
+        document.getElementById("gated-content").classList.remove("hidden");
+
+        document.getElementById(
+            "ipt-access-token"
+        ).innerHTML = await auth0.getTokenSilently();
+
+        document.getElementById("ipt-user-profile").textContent = JSON.stringify(
+            await auth0.getUser()
+        );
+
+    } else {
+        document.getElementById("gated-content").classList.add("hidden");
+    }
+  };
+
+  const login = async () => {
+    await auth0.loginWithRedirect({
+      redirect_uri: window.location.origin
+    });
+  };
+
+  const logout = () => {
+      auth0.logout({
+          returnTo: window.location.origin
+      });
   };
