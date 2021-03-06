c462c8c50965e4a9b2faf873c01cb41d9e0e4b16
Lee Marvin <ryenem8@gmail.com>
2021-03-06 14:36:41 -0800

    setup backend API

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const authConfig = require("./auth_config.json")
 const { join } = require("path");
 const app = express();
 
+
 // Serve static assets from the /public folder
 app.use(express.static(join(__dirname, "public")));
+
+const checkJwt = jwt({
+    secret:jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwkUri: 'https://${authConfig.domain}/.well-known/jwks.json'
+    }),
+
+    audience: authConfig.audience,
+    issuer: 'https://${authConfig.domain}/',
+    algorithms: ["RS256"]
+});
+
+
+app.get("/api/external", checkJwt, (req, res) => {
+    res.send({
+        msg:"Your access token was successfully validated!"
+    });
+});
 
 // Endpoint to serve the configuration file
 app.get("/auth_config.json", (req, res) => {
@@ -13,6 +37,14 @@ app.get("/auth_config.json", (req, res) => {
 // Serve the index page for all other requests
 app.get("/*", (_, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+app.use(function(err, req, res, next){
+    if(err.name === "UnauthorizedError") {
+        return res.status(401).send({ msg: "Invalid token"});
+    }
+
+    next(err, req, res);
 });
 
 // Listen on port 3000
