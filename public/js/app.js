90376b0e840f2a06889e22f218ae02f94a041947
Lee Marvin <ryenem8@gmail.com>
2021-03-06 13:39:56 -0800

    initializing the SDK

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+let auth0 = null;
+
+const fetchAuthConfig = () => fetch("/auth_config.json");
+
+const configureClient = async () => {
+    const response = await fetchAuthConfig();
+    const config = await response.json();
+  
+    auth0 = await createAuth0Client({
+      domain: config.domain,
+      client_id: config.clientId
+    });
+  };
+
+  
+window.onload = async () => {
+    await configureClient();
+
+    updateUI();
+
+  };
+
+  const updateUI = async () => {
+    const isAuthenticated = await auth0.isAuthenticated();
+  
+    document.getElementById("btn-logout").disabled = !isAuthenticated;
+    document.getElementById("btn-login").disabled = isAuthenticated;
+  };
